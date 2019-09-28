b44c66ea0b23200d615cde1e355a652e646edba6
Dash <malasha601@gmail.com>
2019-09-28 15:29:22 +0300

    Initial Commit

---
+++ dz 2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  dz 2
+//
+//  Created by Дарья Ерешко on 28/09/2019.
+//  Copyright © 2019 Дарья Ерешко. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
